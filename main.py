e50a2519aa29b2709a6267dfb2678602a3b0493a
jgray4183 <j.gray.4183@gmail.com>
2024-11-11 15:22:29 +0000

    didn't commit all project oopsy

---
+++ main.py
@@ -0,0 +1,43 @@
+input = "books/frankenstein.txt"
+
+with open(f"{input}") as f:
+    file_contents = f.read()
+
+def main(book):
+    letter_count = characters_decending(count_characters(book))
+    print (letter_count)
+    print (f"--- Begin report for {input} ---")
+    print (f"{count_words(book)} words fount in the docuemnt\n")
+    for letter in letter_count:
+        print (f"The {letter["character"]} character was found {letter["num"]} times")
+    print ("--- End report ---")
+
+def count_words(book):
+    words = book.split()
+    return(len(words))
+
+def count_characters (book):
+    letter_count = {}
+    for letters in book:
+        lowered_letter = letters.lower()
+        if lowered_letter in letter_count:
+            letter_count[lowered_letter] += 1
+        else:
+            letter_count[lowered_letter] = 1
+    return (letter_count)
+
+def sort_on(dict):
+    return dict["num"]
+
+def characters_decending (characters_count):
+    letters_only = []
+    for characters in characters_count:
+        if characters.isalpha():
+            letter_temp = {}
+            letter_temp["character"] = characters
+            letter_temp["num"] = characters_count[characters]
+            letters_only.append(letter_temp)
+    letters_only.sort(reverse=True, key=sort_on)
+    return (letters_only)
+
+main (file_contents)
